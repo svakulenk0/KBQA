e6f542eeee72cdd87eb5a7d44bf5f4c0b307784b
vendi <svitlana.vakulenko@gmail.com>
2018-08-23 10:25:16 +0200

    fix: debug

--- kbqa_rgcn.py
+++ kbqa_rgcn.py
@@ -126,6 +126,7 @@ class KBQA_RGCN:
         X_in = Input(shape=(num_entities,), sparse=True)
 
         kb_encoder_input = [X_in] + A_in
+        input=[X_in] + A_in
 
         # E'' - KB entities initial embedding
         # entity_embedding = 
@@ -168,7 +169,7 @@ class KBQA_RGCN:
 
         answer_decoder_output = decoder_softmax(question_encoder_output)
 
-        self.model_train = Model(question_encoder_input + kb_encoder_input,   # [input question, input KB],
+        self.model_train = Model(input=[question_encoder_input] +[X_in] + A_in,   # [input question, input KB],
                                  answer_decoder_output)                        # ground-truth target answer
 
         def train(self, batch_size, epochs, batch_per_load=10, lr=0.001):
