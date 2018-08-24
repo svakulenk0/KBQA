283066f7a3a7550185966f794068f104d3b33a08
vendi <svitlana.vakulenko@gmail.com>
2018-08-24 18:02:52 +0200

    new: reshape encoder output

--- kbqa_translation.py
+++ kbqa_translation.py
@@ -203,7 +203,7 @@ class KBQA_Translation:
 
         # reshape question_encoder_output to the answer embedding vector size
         # answer_output = Reshape((self.kb_embeddings_dimension,), input_shape=(self.max_seq_len, self.rnn_units))(question_encoder_output)
-        answer_output = Flatten()(question_encoder_output)
+        answer_output = Flatten(input_shape=(self.max_seq_len, self.rnn_units))(question_encoder_output)
 
         # self.model_train = Model([question_encoder_input] +[X_in] + A_in,   # [input question, input KB],
         self.model_train = Model(question_input,   # [input question, input KB],
