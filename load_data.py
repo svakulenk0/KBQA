7b40776e5122f9d7d0499810aa7ffce0f829cd2f
vendi <svitlana.vakulenko@gmail.com>
2018-08-28 15:46:52 +0200

    new: load test split for lc-quad

--- load_data.py
+++ load_data.py
@@ -27,21 +27,25 @@ def load_dbnqa_answers():
     pass
 
 
-def load_lcquad_answers():
+def load_lcquad_answers(split):
     '''
     Load answers for questions from LCQUAD dataset using SPARQL from an endpoint
+    split <Str> train or test
     '''
-    training_questions_path = LCQUAD_DATASET_PATH+'/train-data.json'
-    
-    # download questions dataset
+    # prepare folder for the dataset
     if not os.path.exists(LCQUAD_DATASET_PATH):
         os.makedirs(LCQUAD_DATASET_PATH)
-        wget.download('https://raw.githubusercontent.com/AskNowQA/LC-QuAD/data/train-data.json', training_questions_path)
 
-    with open(training_questions_path) as f:
+    questions_path = LCQUAD_DATASET_PATH + '/%s-data.json' % split
+
+    # download questions dataset
+    if not os.path.exists(questions_path):
+        wget.download('https://raw.githubusercontent.com/AskNowQA/LC-QuAD/data/%s-data.json'%split, questions_path)
+
+    with open(questions_path) as f:
         questions = json.load(f)
 
-    with open("./data/lcquad_train_answers_5000.txt", "w") as f:
+    with open("./data/lcquad_%s_answers.txt"%split, "w") as f:
 
         qas = []
         for question in questions:
@@ -67,11 +71,12 @@ def load_lcquad_answers():
                     f.write('<' + answers[0].encode('utf-8') + '>\n')
                     qas.append({'question': question_str, 'answers': answers})
 
-    with open("./data/lcquad_train.json", "w") as f:
+    with open("./data/lcquad_%s.json"%split, "w") as f:
         json.dump(qas, f, indent=2)
 
 
 if __name__ == '__main__':
     if not os.path.exists(DATASET_PATH):
         os.makedirs(DATASET_PATH)
-    load_lcquad_answers()
+    # load_lcquad_answers('train')
+    load_lcquad_answers('test')
