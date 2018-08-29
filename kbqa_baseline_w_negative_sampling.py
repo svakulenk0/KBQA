9069606881b7fd0b4c1693677b9bb2c7b7af4310
vendi <svitlana.vakulenko@gmail.com>
2018-08-29 14:53:03 +0200

    chg: custom loss for negative sampling

--- kbqa_baseline_w_negative_sampling.py
+++ kbqa_baseline_w_negative_sampling.py
@@ -282,9 +282,9 @@ class KBQA:
             y_true = K.l2_normalize(y_true, axis=-1)
             y_pred = K.l2_normalize(y_pred, axis=-1)
             # print("Batch size: %s" % str(y_pred.shape))
-            batch_size = K.shape(y_pred)[0]
+            size = K.variable(K.shape(y_pred)[0]/2)
             indicator = K.variable(value=[1, -1])
-            loss_vector = -K.sum(y_true * y_pred, axis=-1) * K.tile(indicator, batch_size/2)
+            loss_vector = -K.sum(y_true * y_pred, axis=-1) * K.tile(indicator, size)
             print("Loss: %s" % str(loss_vector.shape))
             return loss_vector
         return loss
