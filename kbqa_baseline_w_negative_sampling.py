d36d8f99fa39aec479f82c3a89dcb586f57648de
vendi <svitlana.vakulenko@gmail.com>
2018-08-29 16:58:16 +0200

    new: add n negative samples

--- kbqa_baseline_w_negative_sampling.py
+++ kbqa_baseline_w_negative_sampling.py
@@ -307,6 +307,7 @@ class KBQA:
             y_pred = K.l2_normalize(y_pred, axis=-1)
             
             loss_vector = -K.sum(y_true * y_pred, axis=-1) * y_indicator
+            K.print_tensor(loss_vector, message="loss vector")
             return loss_vector
         return loss
 
