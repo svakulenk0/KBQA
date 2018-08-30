14992705a4d2b8784ced2641c52e2d15c79140a1
vendi <svitlana.vakulenko@gmail.com>
2018-08-30 15:54:16 +0200

    fix: debug

--- utils.py
+++ utils.py
@@ -110,6 +110,7 @@ def loadKB(kb_entity_labels_list=DBPEDIA_1M_TRIPLES_ENTITIES_LIST, kb_adjacency_
 
     with open(kb_entity_labels_list, 'rb') as f:
         for entity_label in f:
+            print(entity_label)
             keras_idx = idx + 1  # mask 0 for padding
             entityToIndex[entity_label] = keras_idx
     
