bc67724b2754592fb686e20e2856cf0a6e184167
vendi <svitlana.vakulenko@gmail.com>
2018-08-30 10:17:22 +0200

    new: rgcn kbqa architecture

---
+++ rgcn_kbqa2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on Aug 30, 2018
+
+.. codeauthor: svitlana vakulenko
+    <svitlana.vakulenko@gmail.com>
+
+Neural network model with R-GCN layer for KBQA
+
+Based on Keras implementation of RGCN layer https://github.com/tkipf/relational-gcn
+
+Question - text as the sequence of words (word index)
+Answer - entity from KB (entity index)
+
+'''
+import sys
+
+from rgcn_settings import *
+from utils import *
+
+class KBQA_RGCN:
+    '''
+    NN model for KBQA with R-GCN for KB embedding training
+    '''
+    def __init__(self):
+        # load word embeddings with its vocabulary
+        self.wordToIndex, self.indexToWord, self.wordToGlove = readGloveFile()
+
+    def load_data(self, dataset):
+        questions, answers = dataset
+        num_samples = len(questions)
+        assert num_samples == len(answers)
+
+        # encode questions with word vocabulary and answers with entity vocabulary
+        questions_data = []
+        answers_data = []
+
+        # iterate over samples
+        for i in range(num_samples):
+            # encode words of the question (ignore OOV words)
+            questions_sequence = [self.wordToIndex[word] for word in text_to_word_sequence(questions[i]) if word in self.wordToIndex]
+
+
+def main(mode):
+    model = KBQA_RGCN()
+
+    # load data
+    dataset = load_dataset(dataset_name)
+    model.load_data(dataset)
+
+
+if __name__ == '__main__':
+    set_random_seed()
+    main(sys.argv[1])
