c55f019bd25efc3da11c2171f99f485b4120021d
vendi <svitlana.vakulenko@gmail.com>
2018-08-30 15:01:05 +0200

    fix: import

--- rgcn_kbqa2.py
+++ rgcn_kbqa2.py
@@ -23,7 +23,7 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.models import Model
 from keras.models import load_model
 
-from keras.layers import Input, GRU, Dropout, Embedding, Dense, Flatten
+from keras.layers import Input, GRU, Dropout, Embedding, Dense, Flatten, Dot
 from keras.callbacks import  ModelCheckpoint, EarlyStopping
 from keras.regularizers import l2
 from keras.optimizers import Adam
