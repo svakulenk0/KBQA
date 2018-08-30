4f10929e7337db02fb989a05a4f2c274d2c6571d
vendi <svitlana.vakulenko@gmail.com>
2018-08-30 21:15:16 +0200

    chg: debug

--- rgcn_kbqa2.py
+++ rgcn_kbqa2.py
@@ -148,7 +148,8 @@ class KBQA_RGCN:
 
         kb_entities = np.random.randint(low=0, high=1, size=(4, self.num_entities))
         # kb_entities_input = Input(tensor=K.variable(kb_entities))
-        kb_entities_input = Input(tensor=K.variable(np.ones((10, 5))))
+        # TODO
+        kb_entities_input = Input(tensor=K.constant(np.ones((10, 5))))
         
         # E'' - KB entity embedding for entity labels using the same pre-trained word embeddings
         # kb_entities_words_embedding_output = words_embeddings(kb_entities_input)
