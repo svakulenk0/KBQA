d2c441649d31dac97dfe7d7a932567c1609e4caf
vendi <svitlana.vakulenko@gmail.com>
2018-08-30 10:17:44 +0200

    new: rgcn kbqa architecture

---
+++ rgcn_settings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on Aug 30, 2018
+
+.. codeauthor: svitlana vakulenko
+    <svitlana.vakulenko@gmail.com>
+'''
+dataset_name = 'lcquad'
