ba3baf2c63a3b42525eebe8b3bf68d6370a3b8f9
vendi <svitlana.vakulenko@gmail.com>
2018-08-30 19:41:43 +0200

    chg: debug

--- rgcn_kbqa2.py
+++ rgcn_kbqa2.py
@@ -135,16 +135,12 @@ class KBQA_RGCN:
 
         # K - KB input: entities as sequences of words and relations as adjacency matrix
         # https://github.com/tkipf/relational-gcn
-        kb_adjacency_input = []
-        for kb_relation_adjacency in self.kb_adjacency:
-            # kb_relation_adjacency_var = K.variable([1,2,3])
-            kb_relation_adjacency_var = K.variable(kb_relation_adjacency)
-            kb_adjacency_input.append(InputAdj(tensor=kb_relation_adjacency_var))
-
+        # kb_adjacency_input = [InputAdj(tensor=K.variable(kb_relation_adjacency)) for kb_relation_adjacency in self.kb_adjacency]
+        kb_adjacency_input = [K.variable(kb_relation_adjacency) for kb_relation_adjacency in self.kb_adjacency]
         # represent KB entities with 1-hot encoding vectors
             # kb_entities = sp.csr_matrix(self.kb_adjacency[0].shape)
-        kb_entities = K.random_uniform_variable(shape=(self.kb_adjacency[0].shape[0], 4), low=0, high=1)
-        kb_entities_input = Input(tensor=kb_entities, shape=(self.num_entities,))
+        kb_entities_input = K.random_uniform_variable(shape=(self.num_entities, 4), low=0, high=1)
+        # kb_entities_input = Input(tensor=kb_entities, shape=(self.num_entities,))
         
         # E'' - KB entity embedding for entity labels using the same pre-trained word embeddings
         # kb_entities_words_embedding_output = words_embeddings(kb_entities_input)
