aaa0bbcaf287ee6cce4e8a61f8d28c561dab5f76
vendi <svitlana.vakulenko@gmail.com>
2018-08-31 10:03:19 +0200

    chg: debug float64

--- rgcn_kbqa2.py
+++ rgcn_kbqa2.py
@@ -131,7 +131,7 @@ class KBQA_RGCN:
         # https://github.com/tkipf/relational-gcn
         # TODO make tensor out of constant
         kb_entities_input = Input(shape=(self.num_entities,), name='entities_input')
-        kb_adjacency_input = [K.variable(kb_relation_adjacency) for kb_relation_adjacency in self.kb_adjacency]
+        kb_adjacency_input = [K.variable(kb_relation_adjacency, dtype=K.floatx()) for kb_relation_adjacency in self.kb_adjacency]
 
         # E' - question words embedding
         question_embedding_output = words_embeddings(question_input)
