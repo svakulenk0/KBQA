2ad1937ced217924079078e19ef94ba611b408f5
vendi <svitlana.vakulenko@gmail.com>
2018-08-31 18:42:15 +0200

    chg: debug

--- rgcn_kbqa2.py
+++ rgcn_kbqa2.py
@@ -166,7 +166,7 @@ class KBQA_RGCN:
         # kb_projection_output = (question_encoder_output, kb_encoder_output)
 
         # A - answer output
-        # answers_output = Dense(self.num_entities, activation="sigmoid")(kb_encoder_output)
+        answers_output = Dense(self.num_entities, activation="sigmoid")(kb_encoder_output)
 
         self.model_train = Model(inputs=[question_input],   # input question TODO input KB
                                  outputs=[kb_encoder_output])  # ground-truth target answer set
