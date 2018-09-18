d698d61aa37493eb67e806399898ee95b93f4c60
vendi <svitlana.vakulenko@gmail.com>
2018-09-18 14:03:05 +0200

    chg: set limit to load less entities

--- utils.py
+++ utils.py
@@ -126,6 +126,7 @@ def loadKB(kb_entity_labels_list=ENTITIES_LIST, kb_adjacency_path=ADJACENCY_MATR
     with open(kb_adjacency_path, 'rb') as f:
         data = pkl.load(f)
         kb_adjacency = data['A']
+        print(kb_adjacency.shape)
 
     return entityToIndex, kb_adjacency
 
