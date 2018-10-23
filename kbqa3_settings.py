b1aa43fb3728ea265f58435bf5f13d419dd0f9e8
vendi <svitlana.vakulenko@gmail.com>
2018-10-23 14:20:22 +0200

    new: embed questions with fastText

---
+++ kbqa3_settings.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on Oct 18, 2018
+
+.. codeauthor: svitlana vakulenko
+    <svitlana.vakulenko@gmail.com>
+'''
+dataset_name = 'lcquad'
+
+# architecture parameters
+train_word_embeddings = True
+train_kg_embeddings = True
+
+# Question encoder GRU parameters
+rnn_units = 500  # dimension of the GRUs output layer for the hidden question representation
+
+# training parameters
+batch_size = 32
+epochs = 50  # 10
+learning_rate = 1e-3
