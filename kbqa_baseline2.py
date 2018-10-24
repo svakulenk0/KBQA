6bafc77dbf61f025782d1d179cc3686fdc1dd0d6
vendi <svitlana.vakulenko@gmail.com>
2018-10-24 19:08:44 +0200

    chg: increase rnn hidden size

--- kbqa_baseline2.py
+++ kbqa_baseline2.py
@@ -218,7 +218,7 @@ class KBQA:
                 answers_indices.append(answer_indices)
                 # if answer_indices:
                 questions_data.append(questions_sequence)
-                # samples_indicators.append(1)
+                samples_indicators.append(1)
 
 
             # else:
