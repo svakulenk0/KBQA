f5433ca90053fecfd1110eb1a5b237bce79beeed
vendi <svitlana.vakulenko@gmail.com>
2018-10-26 13:41:17 +0200

    new: preprocess entity ids

--- kbqa_model4.py
+++ kbqa_model4.py
@@ -68,7 +68,9 @@ class KBQA:
         kg_word_embeddings_matrix = np.zeros((self.num_entities, self.word_embs_dim))  # initialize with zeros (adding 1 to account for masking)
         for entity_id, index in self.entityToIndex.items():
             # print index, entity_id
-            kg_word_embeddings_matrix[index, :] = self.wordToVec.get_word_vector(entity_id) # create embedding: item index to item embedding
+            entity = entity_id.split('/')[-1].split('_(')[0]
+            print entity
+            kg_word_embeddings_matrix[index, :] = self.wordToVec.get_word_vector(entity) # create embedding: item index to item embedding
         self.kg_word_embeddings_matrix = np.asarray(kg_word_embeddings_matrix, dtype=K.floatx())
 
     def load_data(self, mode, max_question_words=None, max_answers_per_question=100):
