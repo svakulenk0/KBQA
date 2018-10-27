6b58229254053ade323b52fcd5ff8b2557ea1226
vendi <svitlana.vakulenko@gmail.com>
2018-10-27 11:41:07 +0200

    chg: link entities via word embeddings

--- EL_layer.py
+++ EL_layer.py
@@ -15,8 +15,8 @@ from keras import backend as K
 
 def EntityLinking(question_words_embeddings, kg_word_embeddings_matrix, kg_relation_embeddings_matrix):
 
-        kg_word_embeddings = K.constant(self.kg_word_embeddings_matrix.T)
-        kg_relation_embeddings = K.constant(self.kg_relation_embeddings_matrix)
+        kg_word_embeddings = K.constant(kg_word_embeddings_matrix.T)
+        kg_relation_embeddings = K.constant(kg_relation_embeddings_matrix)
 
         kg_embedding = K.dot(kg_word_embeddings, kg_relation_embeddings)
 
