728f9b83d87f9ec199623fec625ac2ec60d2ee3c
vendi <svitlana.vakulenko@gmail.com>
2018-10-27 13:14:46 +0200

    fix: output shape

--- EL_layer.py
+++ EL_layer.py
@@ -24,17 +24,22 @@ class EntityLinking(Layer):
         super(EntityLinking, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        # Create a trainable weight variable for this layer.
-        pass
-
-    def call(self, question_words_embeddings, mask=None):
         kg_word_embeddings = K.variable(self.kg_word_embeddings_matrix.T)
         kg_relation_embeddings = K.variable(self.kg_relation_embeddings_matrix)
-        kg_embedding = K.dot(kg_word_embeddings, kg_relation_embeddings)
-        # train word-to-kg embedding
-        # TODO weights
+        self.kg_embedding = K.dot(kg_word_embeddings, kg_relation_embeddings)
+        
+        # Create a trainable weight variable for word-to-kg embedding
+        # self.kernel = self.add_weight(name='kernel', 
+                                      # shape=(input_shape[1], self.output_dim),
+                                      # initializer='uniform',
+                                      # trainable=True)
+
+        super(EntityLinking, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, question_words_embeddings, mask=None):
+        # TODO multiply with weights kernel
         # return K.dot(question_words_embeddings, K.variable(self.kg_word_embeddings_matrix.T))
-        return K.dot(question_words_embeddings, kg_embedding)
+        return K.dot(question_words_embeddings, self.kg_embedding)
 
     def get_output_shape_for(self, input_shape):
         # return (input_shape[0], input_shape[1], 27293)
