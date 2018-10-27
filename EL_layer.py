f932222dff23981c24eb820c5a09e18d573dde39
vendi <svitlana.vakulenko@gmail.com>
2018-10-27 14:45:13 +0200

    new: weights for learning word2kg embeddings

--- EL_layer.py
+++ EL_layer.py
@@ -28,7 +28,6 @@ class EntityLinking(Layer):
         kg_word_embeddings = K.variable(self.kg_word_embeddings_matrix.T)
         kg_relation_embeddings = K.variable(self.kg_relation_embeddings_matrix)
         self.kg_embedding = K.dot(kg_word_embeddings, kg_relation_embeddings)
-        print K.int_shape(self.kg_embedding)
         
         # Create a trainable weight variable for word-to-kg embedding
         self.kernel = self.add_weight(name='kernel', 
@@ -40,11 +39,10 @@ class EntityLinking(Layer):
 
     def call(self, question_words_embeddings, mask=None):
         # multiply with weights kernel
-        print K.int_shape(self.kernel)
         kg_embedding = K.dot(self.kg_embedding, self.kernel)
-        print K.int_shape(kg_embedding)
-        print K.int_shape(question_words_embeddings)
         question_kg_embedding = K.dot(question_words_embeddings, kg_embedding)
+        print K.int_shape(question_kg_embedding)
+        
         return question_kg_embedding
 
     def get_output_shape_for(self, input_shape):
