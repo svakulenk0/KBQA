b9614e6297715ac54f6b0c5969069976a92ed641
vendi <svitlana.vakulenko@gmail.com>
2018-10-27 13:10:10 +0200

    fix: output shape

--- EL_layer.py
+++ EL_layer.py
@@ -16,28 +16,29 @@ from keras.engine.topology import Layer
 
 class EntityLinking(Layer):
 
-    def __init__(self, kg_word_embeddings_matrix, kg_relation_embeddings_matrix, kg_embedding_dim, **kwargs):
+    def __init__(self, kg_word_embeddings_matrix, kg_relation_embeddings_matrix, output_dim, **kwargs):
         self.kg_word_embeddings_matrix = kg_word_embeddings_matrix
         self.kg_relation_embeddings_matrix = kg_relation_embeddings_matrix
-        self.kg_embedding_dim = kg_embedding_dim
+        self.output_dim = output_dim
 
         super(EntityLinking, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        kg_word_embeddings = K.variable(self.kg_word_embeddings_matrix.T)
-        kg_relation_embeddings = K.variable(self.kg_relation_embeddings_matrix)
-        self.kg_embedding = K.dot(kg_word_embeddings, kg_relation_embeddings)
+        # Create a trainable weight variable for this layer.
+        pass
 
     def call(self, question_words_embeddings, mask=None):
+        kg_word_embeddings = K.variable(self.kg_word_embeddings_matrix.T)
+        kg_relation_embeddings = K.variable(self.kg_relation_embeddings_matrix)
+        kg_embedding = K.dot(kg_word_embeddings, kg_relation_embeddings)
         # train word-to-kg embedding
         # TODO weights
         # return K.dot(question_words_embeddings, K.variable(self.kg_word_embeddings_matrix.T))
-        return K.dot(question_words_embeddings, self.kg_embedding)
+        return K.dot(question_words_embeddings, kg_embedding)
 
     def get_output_shape_for(self, input_shape):
         # return (input_shape[0], input_shape[1], 27293)
-        print((input_shape[0], input_shape[1], self.kg_embedding_dim))
-        return (input_shape[0], input_shape[1], self.kg_embedding_dim)
+        return (input_shape[0], input_shape[1], self.output_dim)
 
     # def get_config(self):
     #     pass
