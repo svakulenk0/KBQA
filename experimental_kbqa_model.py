ecc31907db226a2897bacf461da9646bb8e26013
vendi <svitlana.vakulenko@gmail.com>
2018-10-28 15:29:13 +0100

    chg: adam

--- experimental_kbqa_model.py
+++ experimental_kbqa_model.py
@@ -194,8 +194,8 @@ class KBQA:
         # define loss
        
         if self.output_vector == 'embedding':
-            self.model_train.compile(optimizer='rmsprop', loss='cosine_proximity')
-            # self.model_train.compile(optimizer=Adam(lr=lr), loss=my_loss())
+            # self.model_train.compile(optimizer='rmsprop', loss='cosine_proximity')
+            self.model_train.compile(optimizer=Adam(lr=lr), loss=my_loss())
 
         if self.output_vector == 'one-hot':
             self.model_train.compile(optimizer=Adam(lr=lr), loss='categorical_crossentropy', metrics=['accuracy'])
