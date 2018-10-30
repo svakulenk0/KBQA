427bdb8367529679e2a0b12e342e2c20ed8fa0b2
vendi <svitlana.vakulenko@gmail.com>
2018-10-30 20:48:13 +0100

    new: softmax dense output

--- experimental_kbqa_model.py
+++ experimental_kbqa_model.py
@@ -179,9 +179,12 @@ class KBQA:
         answer_decoder_output_2 = GRU(self.rnn_units, name='answer_decoder_2', return_sequences=True)(answer_decoder_output_1)
         answer_decoder_output_3 = GRU(self.rnn_units, name='answer_decoder_3', return_sequences=True)(answer_decoder_output_2)
         answer_decoder_output_4 = GRU(self.rnn_units, name='answer_decoder_4', return_sequences=True)(answer_decoder_output_3)
-        # answer_output = GRU(self.kg_embeddings_dim, name='answer_decoder')(answer_decoder_output_4)
-        answer_output = GRU(self.num_entities, name='answer_decoder')(answer_decoder_output_4)
-
+        answer_output = GRU(self.kg_embeddings_dim, name='answer_decoder')(answer_decoder_output_4)
+        
+        # A - answer
+        # answer_output = answer_decoder_output
+        answer_output = Dense(self.num_entities, activation='softmax')(answer_decoder_output)
+        
         # K - KG projection
         # kg_projection = Lambda(self.kg_projection_layer, name='answer_selection')(question_encoder_output)  # model 3
 
