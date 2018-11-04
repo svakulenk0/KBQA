1db753b34e5c9aa108c6a19d5f747e8c8e4de853
vendi <svitlana.vakulenko@gmail.com>
2018-11-04 23:36:55 +0100

    new: load all lcquad questions

--- test_word_embeddings.py
+++ test_word_embeddings.py
@@ -42,22 +42,49 @@ def produce_word_lists(questions=[test_q], kg_entities_path=kg_entities_path):
     save_words_list(entity_labels, './data/lcquad_train_entities_labels.txt')
 
 
-def test_embeddings(questions=[test_q], correct_question_entities=[correct_entities],
-                    fname_kg='./data/lcquad_train_entities_labels_fasttext.magnitude'):
+def load_lcquad(dataset_split='train'):
+    QS = []
+    ES = []
+
+    with open("./data/lcquad_%s_new.json"%dataset_split, "r") as train_file:
+        qas = json.load(train_file)
+        
+        print ("%d total QA pairs in lcquad %s" % (len(qas), dataset_split))
+
+        for qa in qas:
+            if qa['answers']:
+                QS.append(qa['corrected_question'])
+                ES.append(qa['entities'])
+            else:
+                empty_answer += 1
     
+    print ("%d questions skipped because no answer was found" % empty_answer)
+    return (QS, ES)
+
+
+def test_embeddings(fname_kg='./data/lcquad_train_entities_labels_fasttext.magnitude'):
+    
+    questions, correct_question_entities = load_lcquad()
+
     # load embeddings
     kg_word_embeddings = Magnitude(fname_kg)
 
-    # iterate over questions
+    # keep counters
     i = 0
+    n_entities = 0
+    n_entities_kg = 0
+    hits = 0
+
+    # iterate over questions
     for question in questions:
         # preprocess entities
         correct_question_entity_labels = [entity_uri.strip('\n').strip('/').strip('>').split('/')[-1]for entity_uri in correct_question_entities[i]]
-        print correct_question_entity_labels
+        n_entities += len(correct_question_entity_labels)
 
         # check that correct entities are in our KG
         existing_correct_entities = [entity for entity in correct_question_entity_labels if entity in kg_word_embeddings]
-        print("%d out of %d entities required to answer the question found in the KG"%(len(existing_correct_entities), len(correct_question_entities[i])))
+        n_entities_kg = len(existing_correct_entities)
+        # print("%d out of %d entities required to answer the question found in the KG"%(n_entities_kg, n_entities))
 
         candidates = []
         
@@ -68,15 +95,21 @@ def test_embeddings(questions=[test_q], correct_question_entities=[correct_entit
             # print(top)
             candidates.extend([entity_score[0] for entity_score in top])
         # print candidates
-        hits = set.intersection(set(candidates), set(existing_correct_entities))
-        print hits
+        hits += len(set.intersection(set(candidates), set(existing_correct_entities)))
+        # print hits
         i += 1
 
+    print ("%d questions"%i)
+    print ("%d entities"%n_entities)
+    print ("%d entities in KG"%n_entities_kg)
+    print ("%d entities in candidates"%hits)
+    print ("%d missed entities"%(n_entities_kg-hits))
     # analyse the error
-    print(kg_word_embeddings.distance("director", "direct"))
+    # print(kg_word_embeddings.distance("director", "direct"))
 
 
 if __name__ == '__main__':
     # produce_word_lists()
     # Then generate fastText embeddings for both lists!
+    # load 
     test_embeddings()
