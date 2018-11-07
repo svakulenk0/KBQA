77f5a1a3fcff66634cca3760c33343579e8523a4
vendi <svitlana.vakulenko@gmail.com>
2018-11-07 11:38:11 +0100

    new: test embeddings matrix load

--- load_embeddings.py
+++ load_embeddings.py
@@ -14,11 +14,14 @@ from pymagnitude import *
 
 EMBEDDINGS_PATH = "./data/embeddings/"
 KB_WORD_EMBEDDINGS_PATH = EMBEDDINGS_PATH + 'DBpedia_KGlove_fasttext.magnitude'
-KB_RELATION_EMBEDDINGS_PATH = EMBEDDINGS_PATH + 'DBpediaVecotrs200_20Shuffle.txt'
+# KB_RELATION_EMBEDDINGS_PATH = EMBEDDINGS_PATH + 'DBpediaVecotrs200_20Shuffle.txt'
 
 
 def load_embeddings():
-    kg_word_embeddings = Magnitude(KB_WORD_EMBEDDINGS_PATH)
+    kg_word_vectors = Magnitude(KB_WORD_EMBEDDINGS_PATH)
+    print("loaded %d KG word vectors with %d dimensions" % (len(kg_word_vectors), kg_word_vectors.dim))
+    kg_word_embeddings_matrix = kg_word_vectors.get_vectors_mmap()
+    kg_word_vectors.close()
     # kg_relation_embeddings = Magnitude(KB_RELATION_EMBEDDINGS_PATH)
 
 
