3df3cfa188f4509a9ec6eeadc810004c12e7d535
vendi <svitlana.vakulenko@gmail.com>
2018-11-18 19:14:11 +0100

    new: test KG entities file

---
+++ test_kb.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on Nov 18, 2018
+
+.. codeauthor: svitlana vakulenko
+    <svitlana.vakulenko@gmail.com>
+
+Parse KG entities
+'''
+path = "./data/entitiesWithObjectsURIs.txt"
+
+with open(path, "r") as infile:
+    print len(infile.read())
+    
+    # for line in infile:
+    #     # line template http://creativecommons.org/ns#license;2
+    #     parse = line.split(';')
+    #     entity_uri = ';'.join(parse[:-1])
+    #     count = parse[-1].strip()
+    #     entity_label = entity_uri.strip('/').split('/')[-1].strip('>').lower().strip('ns#')
