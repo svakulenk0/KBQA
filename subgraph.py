cc46811b0f8de18d16f9b5b6309adab2b4986eed
vendi <svitlana.vakulenko@gmail.com>
2018-11-27 16:00:04 +0100

    chg: retrieve uris of entities

--- subgraph.py
+++ subgraph.py
@@ -26,16 +26,19 @@ es = IndexSearch()
 for question, correct_question_entities in samples:
     print (correct_question_entities)
     # pick a seed entity for each question
-    matched_uris = [match['_source']['uri'] for entity_uri in correct_question_entities for match in es.match_entities(entity_uri, match_by='uri') if match['_source']['term_type'] == "terms"]
+    matched_uris = [match['_source']['uri'] for entity_uri in correct_question_entities for match in es.match_entities(entity_uri, match_by='uri') if match['_source']['term_type'] != "predicates"]
     print (matched_uris)
 
     # request subgraph from the API (2 hops from the seed entity)
     # /home/zola/Projects/hdt-cpp-molecules/libhdt/tools/hops -t "<http://dbpedia.org/resource/David_King-Wood>" -p "http://dbpedia.org/" -n 2 /home/zola/Projects/hdt-cpp-molecules/libhdt/data/dbpedia2016-04en.hdt
     hdt_lib_path = "/home/zola/Projects/hdt-cpp-molecules/libhdt/%s"
     p = Popen([hdt_lib_path%"tools/hops", "-t", matched_uris[0], '-p', "http://dbpedia.org/", '-n', '2', hdt_lib_path%'data/dbpedia2016-04en.hdt'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate()
-    # subgraph = call([hdt_lib_path%"tools/hops", "-t", matched_uris[0], '-p', "http://dbpedia.org/", '-n', '2', hdt_lib_path%'data/dbpedia2016-04en.hdt'])
-    print(output)
+    while True:
+      line = p.stdout.readline()
+      if line != '':
+        print (line.rstrip())
+      else:
+        break
 
     # verify subgraph, i.e. all question entities are within the extracted subgraph
 
