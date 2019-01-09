d9c593a3ea37a818c035ff5792e28451a0a7dbbe
vendi <svitlana.vakulenko@gmail.com>
2019-01-09 17:43:21 +0100

    fix: mongo cursor timeout

--- src/MP_benchmark_new.py
+++ src/MP_benchmark_new.py
@@ -34,7 +34,7 @@ class Mongo_Connector():
         '''
         Set limit to None to get all docs
         '''
-        cursor = self.col.find({'question_type': {'$ne': 'ASK'}, 'train': True})
+        cursor = self.col.find({'question_type': {'$ne': 'ASK'}, 'train': True}, no_cursor_timeout=True)
         if limit:
             cursor = cursor.limit(limit)
         return cursor
@@ -209,65 +209,65 @@ def hop(top_entities_ids, top_predicates_ids, constrained, verbose=False):
 limit = None
 verbose = False
 
-samples = mongo.get_sample(limit=limit)
+cursor = mongo.get_sample(limit=limit)
 # samples = [mongo.get_by_id("29")]  # look up sample by serial number
 
 # hold average stats for the model performance over the samples
 ps, rs, fs = [], [], []
-
-for doc in samples:
-    print(doc['SerialNumber'])
-    
-    if verbose:
-        print(doc['question'])
-        print(doc['sparql_query'])
-        print(doc["1hop"])
-        print(doc["2hop"])
-    
-    assert doc['train'] == True
-    
-    top_entities_ids1 = doc['1hop_ids'][0]
-    top_predicates_ids1 = doc['1hop_ids'][1]
-    answers_ids, answers_uris = hop(top_entities_ids1, top_predicates_ids1, constrained=True)
-
-    _2hops = doc['2hop'] != [[], []]
-    if _2hops:
-        top_entities_ids2 = doc['2hop_ids'][0]
-        top_predicates_ids2 = doc['2hop_ids'][1]
-        answers_ids, answers_uris = hop(top_entities_ids2+answers_ids, top_predicates_ids2, constrained=False)
+with cursor:
+    for doc in cursor:
+        print(doc['SerialNumber'])
         
-    # error estimation
-    answers_uris = set(answers_uris)
-    n_answers = len(answers_uris)
-    gs_answer_uris = set(doc['answers'])
-    n_gs_answers = len(gs_answer_uris)
-    n_correct = len(answers_uris & gs_answer_uris)
+        if verbose:
+            print(doc['question'])
+            print(doc['sparql_query'])
+            print(doc["1hop"])
+            print(doc["2hop"])
+        
+        assert doc['train'] == True
+        
+        top_entities_ids1 = doc['1hop_ids'][0]
+        top_predicates_ids1 = doc['1hop_ids'][1]
+        answers_ids, answers_uris = hop(top_entities_ids1, top_predicates_ids1, constrained=True)
 
-    if verbose:
-        print("%d predicted answers:"%n_answers)
-        print(answers_uris)
-        print("%d gs answers:"%n_gs_answers)
-        print(gs_answer_uris)
-        print(n_correct)
+        _2hops = doc['2hop'] != [[], []]
+        if _2hops:
+            top_entities_ids2 = doc['2hop_ids'][0]
+            top_predicates_ids2 = doc['2hop_ids'][1]
+            answers_ids, answers_uris = hop(top_entities_ids2+answers_ids, top_predicates_ids2, constrained=False)
+            
+        # error estimation
+        answers_uris = set(answers_uris)
+        n_answers = len(answers_uris)
+        gs_answer_uris = set(doc['answers'])
+        n_gs_answers = len(gs_answer_uris)
+        n_correct = len(answers_uris & gs_answer_uris)
 
-    try:
-        r = float(n_correct) / n_gs_answers
-    except ZeroDivisionError:\
-        print(doc['question'])
-    try:
-        p = float(n_correct) / n_answers
-    except ZeroDivisionError:
-        p = 0
-    try:
-        f = 2 * p * r / (p + r)
-    except ZeroDivisionError:
-        f = 0
-    print("P: %.2f R: %.2f F: %.2f"%(p, r, f))
-    
-    # add stats
-    ps.append(p)
-    rs.append(r)
-    fs.append(f)
+        if verbose:
+            print("%d predicted answers:"%n_answers)
+            print(answers_uris)
+            print("%d gs answers:"%n_gs_answers)
+            print(gs_answer_uris)
+            print(n_correct)
+
+        try:
+            r = float(n_correct) / n_gs_answers
+        except ZeroDivisionError:\
+            print(doc['question'])
+        try:
+            p = float(n_correct) / n_answers
+        except ZeroDivisionError:
+            p = 0
+        try:
+            f = 2 * p * r / (p + r)
+        except ZeroDivisionError:
+            f = 0
+        print("P: %.2f R: %.2f F: %.2f"%(p, r, f))
+        
+        # add stats
+        ps.append(p)
+        rs.append(r)
+        fs.append(f)
 
 
 print("\nFin. Results for %d questions:"%len(ps))
