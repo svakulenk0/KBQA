bb9739e3aeabff2aa31788f081c3cb4c475654fa
vendi <svitlana.vakulenko@gmail.com>
2019-01-23 20:25:37 +0100

    fix: index wikidata entities

--- util/index_wikidata.py
+++ util/index_wikidata.py
@@ -24,72 +24,75 @@ curl -XGET "localhost:9200/wikidata201809e/_count"
 '''
 
 # index entities
+
+KB = 'wikidata201809'
+file_path = "../data/KB/terms_wikidata.txt"
+ns_filter = "http://www.wikidata.org"
+ENDPOINT = 'https://www.wikidata.org/w/api.php?action=wbgetentities&props=labels&languages=en&format=json&ids=' # WD API
+
 import io
-import string
+import re, string
+import requests
 
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import streaming_bulk
 from elasticsearch import TransportError
 
-# setup
-doc_type = 'terms'  # for mapping
-KB = 'wikidata201809'
-index_name = '%se' % KB
-file_path = "../data/KB/terms_wikidata.txt"
-ns_filter = "http://www.wikidata.org/entity"
+# pre-processing
+def parse_label(entity_label):
+    entity_label = " ".join(re.sub('([a-z])([A-Z])', r'\1 \2', entity_label.rstrip().lstrip()).split())
+    words = entity_label.split(' ')
+    unique_words = []
+    for word in words:
+        # strip punctuation
+        word = "".join([" " if c in string.punctuation else c for c in word])
+        if word:
+            word = word.lower()
+            if word not in unique_words:
+                unique_words.append(word)
+    entity_label = " ".join(unique_words)
+    return entity_label
 
+
+# setup
+es = Elasticsearch()
+doc_type = 'terms'  # for mapping
 
 # define streaming function
 def uris_stream(index_name, file_path, doc_type, ns_filter=None):
     with io.open(file_path, "r", encoding='utf-8') as infile:
         for i, line in enumerate(infile):
-            # skip URIs if there is a filter set
-            if ns_filter:
-                if not line.startswith(ns_filter):
-                    continue
-            # line template http://www.wikidata.org/entity/P1660;101;has index case
-            parse = line.split(';')
-            
-            entity_label = parse[2].strip()
-            
-            # skip malformed URIs
-            if not entity_label or len(entity_label) > 150:
+            if ns_filter and not line.startswith(ns_filter):
                 continue
-
-            # label preprocessing: remove punctuation, duplicate words, lowercase
-            words = entity_label.split(' ')
-            unique_words = []
-            for word in words:
-                # strip punctuation
-                word = "".join([c for c in word if c not in string.punctuation])
-                if word:
-                    word = word.lower()
-                    if word not in unique_words:
-                        unique_words.append(word)
-            entity_label = " ".join(unique_words)
-            
+            parse = line.split(';')
             entity_uri = parse[0]
             count = parse[1]
-            
             wd_id = entity_uri.strip('/').split('/')[-1]  # part of the WD URI
-        
+            entity_label = parse[2].strip()
+            if not entity_label:
+                response = requests.get(ENDPOINT+wd_id).json()['entities'][wd_id]
+                if 'labels' in response:
+                    labels = response['labels']
+                    if labels:
+                        entity_label = labels['en']['value']
+            entity_label = parse_label(entity_label)
+            
             data_dict = {'uri': entity_uri, 'label': entity_label,
                          'count': count, "id": i+1, 'label_exact': wd_id}
-            
-            print(data_dict)
 
             yield {"_index": index_name,
                    "_type": doc_type,
                    "_source": data_dict
                    }
 
+# index entities
+index_name = '%se' % KB  # entities index
 
-es = Elasticsearch()
 # iterate through input file in batches via streaming bulk
 print("bulk indexing...")
 try:
     for ok, response in streaming_bulk(es, actions=uris_stream(index_name, file_path, doc_type, ns_filter),
-                                        chunk_size=200000):
+                                       chunk_size=100000):
         if not ok:
             # failure inserting
             print (response)
