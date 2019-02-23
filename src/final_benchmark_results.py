8376e93e4640e87d7e956f261ffc80c473a49b4d
vendi <svitlana.vakulenko@gmail.com>
2019-02-23 19:58:47 +0100

    new: results benchmarK

--- src/final_benchmark_results.py
+++ src/final_benchmark_results.py
@@ -22,7 +22,7 @@ mongo = Mongo_Connector('kbqa', dataset_name)
 from hdt import HDTDocument
 hdt_path = "/home/zola/Projects/hdt-cpp-molecules/libhdt/data/"
 hdt_file = 'dbpedia2016-04en.hdt'
-namespace = "http://dbpedia.org/"
+namespace = "predef-dbpedia2016-04"  # "http://dbpedia.org/"
 
 from collections import defaultdict
 import numpy as np
