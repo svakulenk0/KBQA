3ecf1839814247fef2a52769fd00dabf01f88343
vendi <svitlana.vakulenko@gmail.com>
2019-05-19 11:27:01 +0300

    new: measure size and time per hop

--- src/final_benchmark_results_time_size2.py
+++ src/final_benchmark_results_time_size2.py
@@ -325,7 +325,7 @@ def hop(entities, constraints, top_predicates, verbose=False, max_triples=500000
 from collections import Counter
 
 verbose = False
-limit = 2
+limit = 5
 
 question_types = ['SELECT', 'ASK', 'COUNT']
 
