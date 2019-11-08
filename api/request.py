5bb5cc45261f0f852c999127df511d71fa665351
vendi <svitlana.vakulenko@gmail.com>
2019-11-08 15:33:44 +0100

    fix: model path

--- api/request.py
+++ api/request.py
@@ -43,13 +43,13 @@ class KBQA():
         self.p_vectors = load_embeddings('fasttext_p_labels')
         
         # load pre-trained question type classification model
-        with open(model_path+'%s_%s_%s.pkl'%(modelname, dataset_name, embeddings_choice), 'rb') as f:
+        with open(model_path+'qtype_lcquad_%s.pkl'%(embeddings_choice), 'rb') as f:
             self.model_settings = pkl.load(f)
         self.qt_model = build_qt_inference_model(self.model_settings)
         self.qt_model.load_weights(model_path+'checkpoints/_qtype_weights.best.hdf5', by_name=True)
 
         # load pre-trained question parsing model
-        with open(model_path+'%s_%s.pkl'%(dataset_name, embeddings_choice), 'rb') as f:
+        with open(model_path+'lcquad_%s.pkl'%(embeddings_choice), 'rb') as f:
             ep_model_settings = pkl.load(f)
         self.ep_model = build_ep_inference_model(ep_model_settings)
         # load weights
