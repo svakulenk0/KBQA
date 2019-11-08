ce8d8761127520c2f7095ff6b6ac06c2b7249870
vendi <svitlana.vakulenko@gmail.com>
2019-11-08 15:45:06 +0100

    fix: structure

--- api/request.py
+++ api/request.py
@@ -64,14 +64,14 @@ class KBQA():
         self.kg = HDTDocument(hdt_path+hdt_file)
 
     # functions for entity linking and relation detection
-    def entity_linking(e_spans, verbose=False, cutoff=500, threshold=0): 
+    def entity_linking(self, e_spans, verbose=False, cutoff=500, threshold=0): 
         guessed_ids = []
         for span in e_spans:
             span_ids = self.e_index.label_scores(span, top=cutoff, threshold=threshold, verbose=verbose, scale=0.3, max_degree=50000)
             guessed_ids.append(span_ids)
         return guessed_ids
 
-    def relation_detection(p_spans, verbose=False, cutoff=500, threshold=0.0): 
+    def relation_detection(self, p_spans, verbose=False, cutoff=500, threshold=0.0): 
         guessed_ids = []
         for span in p_spans:
             span_ids = {}
@@ -91,6 +91,122 @@ class KBQA():
                         print(score)
             guessed_ids.append(span_ids)
         return guessed_ids
+
+    def hop(self, entities, constraints, top_predicates, verbose=False, max_triples=500000, bl_p=[68655]):
+        '''
+        Extract the subgraph for the selected entities
+        bl_p  -- the list of predicates to ignore (e.g. type predicate is too expensive to expand)
+        ''' 
+    #     print(top_predicates)
+        n_constraints = len(constraints)
+        if entities:
+            n_constraints += 1
+
+        top_entities = entities + constraints
+        all_entities_ids = [_id for e in top_entities for _id in e]
+        top_predicates_ids = [_id for p in top_predicates for _id in p if _id not in bl_p]
+
+        # iteratively call the HDT API to retrieve all subgraph partitions
+        activations = defaultdict(int)
+        offset = 0
+
+        while True:
+            # get the subgraph for selected predicates only
+    #         print(top_predicates_ids)
+            self.kg.configure_hops(1, top_predicates_ids, namespace, True)
+            entities, predicate_ids, adjacencies = self.kg.compute_hops(all_entities_ids, max_triples, offset)
+    #         print(adjacencies)
+            # show subgraph entities
+    #         print([e_index.look_up_by_id(e)[0]['_source']['uri'] for e in entities])
+            
+            if not entities:
+                answers = [{a_id: a_score} for a_id, a_score in activations.items()]
+                return answers
+
+            if verbose:
+                print("Subgraph extracted:")
+                print("%d entities"%len(entities))
+                print("%d predicates"%len(predicate_ids))
+                print("Loading adjacencies..")
+
+            offset += max_triples
+            # index entity ids global -> local
+            entities_dict = {k: v for v, k in enumerate(entities)}
+            # generate a list of adjacency matrices per predicate assuming the graph is undirected wo self-loops
+            A = generate_adj_sp(adjacencies, len(entities), include_inverse=True)
+    #         print(predicate_ids)
+            # activate entities -- build sparse matrix
+            row, col, data = [], [], []
+            for i, concept_ids in enumerate(top_entities):
+                for entity_id, score in concept_ids.items():
+                    if entity_id in entities_dict:
+    #                     print(e_index.look_up_by_id(entity_id)[0]['_source']['uri'])
+    #                     print(score)
+                        local_id = entities_dict[entity_id]
+                        row.append(i)
+                        col.append(local_id)
+                        data.append(score)
+            x = sp.csr_matrix((data, (row, col)), shape=(len(top_entities), len(entities)))
+        
+            # iterate over predicates
+            ye = sp.csr_matrix((len(top_entities), len(entities)))
+            # activate predicates
+            if top_predicates_ids:
+                yp = sp.csr_matrix((len(top_predicates), len(entities)))
+                for i, concept_ids in enumerate(top_predicates):
+                    # activate predicates
+                    p = np.zeros([len(predicate_ids)])
+                    # iterate over synonyms
+                    for p_id, score in concept_ids.items():
+                        if p_id in predicate_ids:
+                            local_id = predicate_ids.index(p_id)
+                            p[local_id] = score
+                    # slice A by the selected predicates
+                    _A = sum(p*A)
+                    _y = x @ _A
+                    # normalize: cut top to 1
+                    _y[_y > 1] = 1
+                    yp[i] = _y.sum(0)
+                    ye += _y
+                y = sp.vstack([ye,yp])
+            # fall back to evaluate all predicates
+            else:
+                y = x @ sum(A)
+            sum_a = sum(y)
+            sum_a_norm = sum_a.toarray()[0] / (len(top_predicates) + n_constraints) #normalize(sum_a, norm='max', axis=1).toarray()[0]
+            # normalize: cut top to 1
+            sum_a_norm[sum_a_norm > 1] = 1
+            # activations across components
+            y_counts = binarize(y, threshold=0.0)
+            count_a = sum(y_counts).toarray()[0]
+            # final scores
+            y = (sum_a_norm + count_a) / (len(top_predicates) + n_constraints + 1)
+
+            # check output size
+            assert y.shape[0] == len(entities)
+
+            top = np.argwhere(y > 0).T.tolist()[0]
+            if len(top) > 0:
+                activations1 = np.asarray(entities)[top]
+                # store the activation values per id answer id
+                for i, e in enumerate(entities):
+                    if e in activations1:
+                        activations[e] += y[i]
+            # if not such answer found fall back to return the answers satisfying max of the constraints
+            else:
+                # select answers that satisfy maximum number of constraints
+                y_p = np.argmax(y)
+                # maximum number of satisfied constraints
+                max_cs = y[y_p]
+                # at least some activation (evidence from min one constraint)
+                if max_cs != 0:
+                    # select answers
+                    top = np.argwhere(y == max_cs).T.tolist()[0]
+                    activations1 = np.asarray(entities)[top]
+                    # store the activation values per id answer id
+                    for i, e in enumerate(entities):
+                        if e in activations1:
+                            activations[e] += y[i]
 
     def request(self, question, verbose=False):
         # parse question into words and embed
@@ -117,14 +233,14 @@ class KBQA():
         #         c_spans2 = doc['c2_spans']
 
         # match predicates
-        top_predicates_ids1 = relation_detection(p_spans1, threshold=0)
-        top_predicates_ids2 = relation_detection(p_spans2, threshold=0)
+        top_predicates_ids1 = self.relation_detection(p_spans1, threshold=0)
+        top_predicates_ids2 = self.relation_detection(p_spans2, threshold=0)
 
         # use GS classes
         #         classes1 = [{_id: 1} for _id in doc['classes_ids'] if _id in doc['1hop_ids'][0]]
         #         classes2 = [{_id: 1} for _id in doc['classes_ids'] if _id in doc['2hop_ids'][0]]
 
-        top_entities_ids1 = entity_linking(e_spans1, threshold=0.7)
+        top_entities_ids1 = self.entity_linking(e_spans1, threshold=0.7)
 
         if ask_question:
             a_threshold = 0.0
@@ -135,14 +251,14 @@ class KBQA():
         answers_ids = []
 
         # 1st hop
-        answers_ids1 = hop([], top_entities_ids1, top_predicates_ids1, verbose)
+        answers_ids1 = self.hop([], top_entities_ids1, top_predicates_ids1, verbose)
         #         if classes1:
         #             answers_ids1 = filter_answer_by_class(classes1, answers_ids1)
         answers1 = [{a_id: a_score} for activations in answers_ids1 for a_id, a_score in activations.items() if a_score > a_threshold]
 
         # 2nd hop
         if top_predicates_ids1 and top_predicates_ids2:                
-            answers_ids = hop(answers1, [], top_predicates_ids2, verbose)
+            answers_ids = self.hop(answers1, [], top_predicates_ids2, verbose)
         #             if classes2:
         #                 answers_ids = filter_answer_by_class(classes2, answers_ids)
             answers = [{a_id: a_score} for activations in answers_ids for a_id, a_score in activations.items() if a_score > a_threshold]
