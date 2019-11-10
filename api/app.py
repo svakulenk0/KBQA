5e98562d07e0464a838909c8bc31810a4685f90b
vendi <svitlana.vakulenko@gmail.com>
2019-11-10 14:40:41 +0100

    new: top n parameter in request

--- api/app.py
+++ api/app.py
@@ -9,25 +9,25 @@ Created on Nov 8, 2019
 
 Flask-based RESTful API for KBQA on DBpedia
 '''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+
 from request import KBQA
 
 app = Flask(__name__)
+api = Api(app)
 
+class KBQAPI(Resource):
+    def __init__(self):
+        self.service = KBQA()
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+    def get(self):
+        question = request.args.get('question', type=str)
+        top_n = request.args.get('top_n', default=3, type=int)
+        answers = self.service.request(question, top_n, verbose=False)
+        return jsonify({'answers': answers})
 
-
-@app.route('/ask', methods=['GET'])
-def ask_qamp():
-    question = request.args.get('question', type=str)
-    print(question)
-    service = KBQA()
-    answers = service.request(question, verbose=False)
-    return jsonify({'answers': answers})
-
+api.add_resource(KBQAPI, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
