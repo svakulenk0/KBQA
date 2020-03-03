1d8d4aa7d293ce251b442fc16ba0dbaac9216b7f
vendi <svitlana.vakulenko@gmail.com>
2020-03-03 13:19:38 +0100

    fix: show answer uris

--- src/final_benchmark_results_cache.py
+++ src/final_benchmark_results_cache.py
@@ -444,7 +444,7 @@ with cursor:
                 else:
                     # print(answers_ids)
                     #  if e_index.look_up_by_id(_id)
-                    print([e_index.look_up_by_id(_id)[0]['_source']['uri'] for answer in answers_ids])
+                    print([e_index.look_up_by_id(_id)[0]['_source']['uri'] for _id in answers_ids])
 
                     n_correct = len(answers_ids & gs_answer_ids)
                     try:
